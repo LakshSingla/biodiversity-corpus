98a2c451aa58d8b689a1779336dde5d5d31d00c9
Laksh Singla <lakshsingla@gmail.com>
2019-06-16 02:29:46 +0530

    fetch tweets using tweepy

--- frontend_nontemplate/js/main.js
+++ frontend_nontemplate/js/main.js
@@ -1,10 +1,14 @@
 const mainCategorySelectElem = document.getElementById('main-category-select');
 const subCategorySelectOuterElem = document.getElementById('subcategory-select-outer');
 
+let g_map;          //Global variable for map
+let g_codebird;     //Global variable for CodeBird
+
 const mapping = {
     flora: ['Lily', 'Dahlia'],
     fauna: ['Tiger', 'Bear'],
     hills: ['Rugged'],
+    calamity: ['Earthquake', 'Forest Fire']
 }
 
 const getMainCategoryValue = () => mainCategorySelectElem.value;
@@ -41,7 +45,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let modalInstances = M.Modal.init(elems, {});
 
     populateSubcategorySelect();
-    initMap();
+    g_map = initMap();
+
+    g_codebird = setupCodebird();
+    g_codebird.__call('search_tweets', {
+        q: getSubCategoryValue()
+    }, result => {
+        console.log(result)
+    })
+
 });
 
 mainCategorySelectElem.addEventListener('change', populateSubcategorySelect);
--- frontend_nontemplate/js/twitter.js
+++ frontend_nontemplate/js/twitter.js
@@ -1,11 +1,14 @@
 const TWITTER_BASE_URL = 'https://api.twitter.com/1.1/search/tweets.json'
 
-const cb = new Codebird();
-cb.setConsumerKey(TWITTER_API_KEY, TWITTER_SECRET_KEY);
+const setupCodebird = () => {
+    const cb = new Codebird();
+    cb.setConsumerKey(TWITTER_API_KEY, TWITTER_SECRET_KEY);
+    return cb;
+}
 
-cb.__call('search_tweets', {
-    q: "NYC"
+/*cb.__call('search_tweets', {
+    q: "jammu"
 }, reply => {
     console.log(reply)
     console.log('here')
-});
+});*/
