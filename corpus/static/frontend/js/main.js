d9a034374e4ddb7800b701ce47ae6b8be7486910
Laksh Singla <lakshsingla@gmail.com>
2019-06-26 14:40:40 +0530

    integrate frontend with backend for keyword data

--- corpus/static/frontend/js/main.js
+++ corpus/static/frontend/js/main.js
@@ -1,19 +1,33 @@
 const mainCategorySelectElem = document.getElementById('main-category-select');
+const mainCategorySelectOuterElem = document.getElementById('main-category-select-outer');
 const subCategorySelectOuterElem = document.getElementById('subcategory-select-outer');
 const searchBtn = document.getElementById('search-btn');
 
 let g_map;          //Global variable for map
 let g_codebird;     //Global variable for CodeBird
 
-const mapping = {
-    flora: ['Lily', 'Dahlia'],
-    fauna: ['Tiger', 'Bear'],
-    hills: ['Rugged'],
-    calamity: ['Earthquake', 'Forest Fire']
-}
+let mapping = {};
 
-const getMainCategoryValue = () => mainCategorySelectElem.value;
+const getMainCategoryValue = () => mainCategorySelectOuterElem.querySelector('select').value;
 const getSubCategoryValue = () => subCategorySelectElem.value;
+
+const populateCategorySelect = () => {
+    const _mainCategorySelectElem = mainCategorySelectOuterElem.querySelectorAll('.select-wrapper');
+    _mainCategorySelectElem.forEach(elem => elem.remove())
+    const selectElem = document.createElement('select');
+    const elemArr = [];
+    for(const k in mapping) {
+        const elem = document.createElement('option');
+        elem.setAttribute('value', k);
+        elem.appendChild(document.createTextNode(k));
+        elemArr.push(elem);
+    }
+    selectElem.addEventListener('change', populateSubcategorySelect);
+    elemArr.forEach(elem => selectElem.appendChild(elem))
+    mainCategorySelectOuterElem.prepend(selectElem);
+    let elems = document.querySelectorAll('select');
+    let selectInstances = M.FormSelect.init(elems, {});
+}
 
 const populateSubcategorySelect = () => {
     const subCategorySelectElem = subCategorySelectOuterElem.querySelectorAll('.select-wrapper');
@@ -45,13 +59,22 @@ document.addEventListener('DOMContentLoaded', () => {
     elems = document.querySelectorAll('.modal');
     let modalInstances = M.Modal.init(elems, {});
 
-    populateSubcategorySelect();
+    // populateCategorySelect();
+    // populateSubcategorySelect();
+
+    fetch('/db/categories').then(data => data.json()).then(data => {
+        console.log(data)
+        mapping = data;
+        populateCategorySelect();
+        populateSubCategorySelect();
+    })
+
     g_map = initMap();
 
     g_codebird = setupCodebird();
 });
 
-mainCategorySelectElem.addEventListener('change', populateSubcategorySelect);
+// mainCategorySelectElem.addEventListener('change', populateSubcategorySelect);
 searchBtn.addEventListener('click', () => {
     g_codebird.__call('search_tweets', {
         q: getSubCategoryValue()
