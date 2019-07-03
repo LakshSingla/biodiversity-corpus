f2348281ae1a1e9d01a3fea7ad46c6de28666f4b
Laksh Singla <lakshsingla@gmail.com>
2019-07-03 19:57:03 +0530

    xx

--- corpus/tweets/analyse_tweets.py
+++ corpus/tweets/analyse_tweets.py
@@ -3,6 +3,7 @@ from textblob import TextBlob
 from geopy.geocoders import Nominatim, GoogleV3
 from geopy.exc import GeocoderTimedOut
 from geopy.point import Point
+import spacy
 
 INDIAN_BOUNDS = [Point(latitude=35.4940095078, longitude=68.1766451354), Point(latitude=7.96553477623, longitude=97.4025614766)]
 
@@ -29,12 +30,20 @@ def get_tweet_sentiment(tweet):
 def get_location(tweet):
     cleaned_tweet = clean_tweet(tweet)
     geolocator = Nominatim(user_agent="test")
-    google_geolocator = GoogleV3(api_key='AIzaSyDUrM4reXIG5A8GoZEPrKnPuGYIV0Do7dk')
-    try:
-        # location = geolocator.geocode(cleaned_tweet)
-        location = google_geolocator.geocode(query=cleaned_tweet, bounds=INDIAN_BOUNDS)
-    except GeocoderTimedOut:
-        print("Error: geocode failed on input %s" %
-          cleaned_tweet,)
-        return {}
+    # google_geolocator = GoogleV3(api_key='AIzaSyDUrM4reXIG5A8GoZEPrKnPuGYIV0Do7dk')
+    sp = spacy.load('en_core_web_sm') 
+    sen = sp(cleaned_tweet)
+    location = None
+    for entity in sen.ents:
+        # print(entity.text + ' - ' + entity.label_ + ' - ' + str(spacy.explain(entity.label_))) 
+        if entity.label_ == 'LOC':
+            print(f"LOCATION FOUND - {entity.text}")
+            try:
+                location = geolocator.geocode(entity.text)
+            except Exception:
+                pass
+            if location is not None:
+                # pass
+                print(tweet)
+                print(f"{entity.text} - {location.latitude}, {location.longitude}")
     return location
--- corpus/tweets/views.py
+++ corpus/tweets/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 import tweepy
 from tweepy import OAuthHandler
 from .twitter_auth import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-from .analyse_tweets import get_location, get_tweet_sentiment
+from .analyse_tweets import get_location, get_tweet_sentiment, clean_tweet
 
 from functools import reduce
 
@@ -19,15 +19,17 @@ rad = 2000  # km
 def tweets(request, keyword):
     # geolocator = Nominatim(user_agent="test")
     if request.method == 'GET':
-        max_tweets = 100
+        max_tweets = 1000
         searched_tweets = [status._json for status in tweepy.Cursor(
-            api.search, q=f"{keyword} -filter:retweets", lang='en', geocode=f'{lat},{lon},{rad}km').items(max_tweets)]
+            api.search, q=f"{keyword} -filter:retweets", lang='en').items(max_tweets)]
+            # api.search, q=f"{keyword} -filter:retweets", lang='en', geocode=f'{lat},{lon},{rad}km').items(max_tweets)]
         
         pos, neg, neut = 0, 0, 0
 
         for tweet in searched_tweets:
-            print(tweet['created_at'])
+            # print(tweet['created_at'])
             sentiment = get_tweet_sentiment(tweet['text'])
+            get_location(tweet['text'])
             if sentiment == 'positive': pos += 1
             elif sentiment == 'negative': neg += 1
             elif sentiment == 'neutral': neut += 1
@@ -35,7 +37,7 @@ def tweets(request, keyword):
         resp = {
             'tweets': [
                 {
-                    'text': tweet['text'],
+                    'text': clean_tweet(tweet['text']),
                     # 'location': get_location(tweet['text']),
                     'sentiment': get_tweet_sentiment(tweet['text'])
                 } for tweet in searched_tweets],
