0f756c603d6eaff0a2fd4f43a31cd845329bc775
Laksh Singla <lakshsingla@gmail.com>
2019-07-03 17:07:33 +0530

    sentiment analysis added

---
+++ corpus/static/frontend/js/chart.js
@@ -0,0 +1,78 @@
+let ctx;
+ctx = document.getElementById('sentiment-chart').getContext('2d');
+const sentimentChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ['Positive', 'Negative', 'Neutral'],
+        datasets: [{
+            label: 'Sentiments',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [3, 10, 5]
+        }]
+    },
+    options: {
+        responsive: true,
+        elements: {
+            rectangle: {
+                borderWidth: 2,
+            },
+        },
+        scales: {
+            yAxes: [{
+                barThickness: 10,
+            }]
+        }
+
+    }
+
+});
+
+ctx = document.getElementById('timeseries-chart').getContext('2d');
+const timeseries = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [
+            'Jan',
+            'Feb',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'Aug',
+            'Sept',
+            'Oct',
+            'Nov',
+            'Dec',
+        ],
+        datasets: [{
+            label: 'Sentiments',
+            borderColor: 'rgb(255, 99, 132)',
+            fill: false,
+            data: [2, 10, 5, 5, 5, 6, 5, 3, 5, 3, 10, 2]
+        }]
+    },
+
+});
+
+ctx = document.getElementById('statewise-chart').getContext('2d');
+const statewise = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: [
+            'State 1',
+            'State 2',
+            'State 3',
+            'Other',
+        ],
+        datasets: [{
+            label: 'Sentiments',
+            backgroundColor: 'rgb(99, 231, 12)',
+            borderColor: 'rgb(255, 99, 132)',
+            fill: false,
+            data: [2, 10, 5, 5]
+        }]
+    },
+
+});
--- corpus/tweets/views.py
+++ corpus/tweets/views.py
@@ -5,23 +5,50 @@ from tweepy import OAuthHandler
 from .twitter_auth import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 from .analyse_tweets import get_location, get_tweet_sentiment
 
+from functools import reduce
+
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 lat = 22
 lon = 78
-rad = 2000 # km
+rad = 2000  # km
+
 
 def tweets(request, keyword):
     # geolocator = Nominatim(user_agent="test")
     if request.method == 'GET':
-        max_tweets = 10
+        max_tweets = 100
         searched_tweets = [status._json for status in tweepy.Cursor(
             api.search, q=f"{keyword} -filter:retweets", lang='en', geocode=f'{lat},{lon},{rad}km').items(max_tweets)]
-        resp = [{
-            'text': tweet['text'],
-            'location': get_location(tweet['text']),
-            'sentiment': get_tweet_sentiment(tweet['text'])
-        } for tweet in searched_tweets]
+        
+        pos, neg, neut = 0, 0, 0
+
+        for tweet in searched_tweets:
+            print(tweet['created_at'])
+            sentiment = get_tweet_sentiment(tweet['text'])
+            if sentiment == 'positive': pos += 1
+            elif sentiment == 'negative': neg += 1
+            elif sentiment == 'neutral': neut += 1
+
+        resp = {
+            'tweets': [
+                {
+                    'text': tweet['text'],
+                    # 'location': get_location(tweet['text']),
+                    'sentiment': get_tweet_sentiment(tweet['text'])
+                } for tweet in searched_tweets],
+            
+            'analysis': {
+                'sentiment': {
+                    'positive': pos,
+                    'negative': neg,
+                    'neutral': neut
+                },
+                'timeseries': None,
+                'location': None
+            }
+        }
+
         return JsonResponse(resp)
